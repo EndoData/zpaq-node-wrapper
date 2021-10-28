45e42b98bb287cce08ffdcbbbade2041b1444fa2
jeremypatrickdahan <jeremydahan@protonmail.com>
2021-10-28 11:34:11 +0200

    Clean u

--- index.js
+++ index.js
@@ -1,39 +1,61 @@
-const execa = require('execa');
-const path = require('path')
+const execa = require("execa");
+const path = require("path");
 
+let zpaqBinPath = path.join(
+  __dirname.replace("app.asar", "app.asar.unpacked"),
+  "bin",
+  process.platform === "win32" ? "zpaq.exe" : "zpaq"
+);
 
+const listVersions = async function (folderPath, archiveRelativePath) {
+  const { stdout, stderr } = await execa(
+    zpaqBinPath,
+    [
+      "v",
+      archiveRelativePath,
+      "-s1", // Summary level 1 option, which means quiet after our modifications
+    ],
+    { cwd: folderPath, shell: true }
+  );
+  return stdout.split(/\r?\n/);
+};
 
-let zpaqBinPath = path.join(__dirname.replace('app.asar', 'app.asar.unpacked'), 'bin', process.platform === 'win32' ? 'zpaq.exe' : 'zpaq')
+const addFile = async function (
+  folderPath,
+  archiveRelativePath,
+  fileRelativePath
+) {
+  const { stdout, stderr } = await execa(
+    zpaqBinPath,
+    [
+      "add",
+      archiveRelativePath,
+      fileRelativePath,
+      "-s1", // Summary level 1 option, which means quiet after our modifications
+    ],
+    { cwd: folderPath, shell: true }
+  );
+  return stdout.split(/\r?\n/);
+};
 
-const listVersions = async function(folderPath, archiveRelativePath){
-	const { stdout, stderr } = await execa(zpaqBinPath, [
-		'v',
-		archiveRelativePath,
-		'-s1' // Summary level 1 option, which means quiet after our modifications
-	], { cwd : folderPath, shell : true})
-	return stdout.split(/\r?\n/)
-}
+const extractUntil = async function (
+  folderPath,
+  archiveRelativePath,
+  versionDate
+) {
+  const { stdout, stderr } = await execa(
+    zpaqBinPath,
+    [
+      "x",
+      archiveRelativePath,
+      "-until",
+      versionDate,
+      "-s1", // Summary level 1 option, which means quiet after our modifications
+      "-f",
+    ],
+    { cwd: folderPath, shell: true }
+  );
+  return stdout.split(/\r?\n/);
+};
 
-const addFile = async function(folderPath, archiveRelativePath, fileRelativePath){
-	const { stdout, stderr } = await execa(zpaqBinPath, [
-		'add',
-		archiveRelativePath,
-		fileRelativePath,
-		'-s1' // Summary level 1 option, which means quiet after our modifications
-	], { cwd : folderPath, shell : true})
-	return stdout.split(/\r?\n/)
-}
-
-const extractUntil = async function(folderPath, archiveRelativePath, versionDate){
-	const { stdout, stderr } = await execa(zpaqBinPath, [
-		'x',
-		archiveRelativePath,
-		'-until',
-		versionDate,
-		'-s1', // Summary level 1 option, which means quiet after our modifications
-		'-f'
-	], { cwd : folderPath, shell : true})
-	return stdout.split(/\r?\n/)
-}
-
-module.exports = {listVersions, addFile, extractUntil};
+module.exports = { listVersions, addFile, extractUntil };
